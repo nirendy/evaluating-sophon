a3c371e5d1000c22667d5407adbcb288391e43de
nirendy <nirendy@gmail.com>
2024-09-20 23:31:33 +0300

    move files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name='my_package',
+    name='evaluating_sophon',
     version='0.1',
     packages=find_packages(),
     install_requires=[],
